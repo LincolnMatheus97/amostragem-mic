bcf6409989fad30d76dad257fa0cc17ab8d988e3
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-08-06 18:23:53 -0300

    :recycle: refactor: Refatora as funções da utils da matriz de LED

--- src/utils/matriz/matriz.h
+++ src/utils/matriz/matriz.h
@@ -5,10 +5,11 @@
 #include "pico/stdlib.h"
 
 #define LEDS_VERDES 13
-#define LEDS_AMARELOS 7
-#define LEDS_VERMELHOS 5
+#define LEDS_AMARELOS 8
+#define LEDS_VERMELHOS 4
+
 #define MIN_DB_MAPA 30.0f
-#define MAX_DB_MAPA 85.0f
+#define MAX_DB_MAPA 103.0f
 
 #define CANAL_PIO pio0
 #define PINO_MATRIZ 7
